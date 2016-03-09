b27ebc58c36039d475d2ecd4e31284f08c6783a2
Paul Novack <PaulNovack@users.noreply.github.com>
2016-03-08 19:13:59 -0800

    Updated to v0-5-0 w/o long value support using append to key method

--- loganDBClient.class.php
+++ loganDBClient.class.php
@@ -14,7 +14,7 @@ class loganDBClient {
   public $timeout;  // in milliseconds;
   public $keys = array();
   public $values = array();
-  private $apiversion = "0-3-0";   
+  private $apiversion = "0-5-0";   
   private $socket;
   function __construct($serverip = "127.0.0.1",$port = 6779,$authid = "JAVA-default",$sessionid = "JAVA-default",$timeout = 1000) {
      $this->serverip = $serverip;
@@ -25,7 +25,7 @@ class loganDBClient {
      $this->hostport = $this->serverip . ":" . $this->port;
   }
   public function Connect(){
-    $this->sock = @stream_socket_client("$this->hostport", $errno, $errstr, $this->timeout);
+    $this->sock = stream_socket_client("$this->hostport", $errno, $errstr, $this->timeout);
     if(!$this->sock){
       echo "Connection error!";  //throw new loganDBException("$errno: $errstr");
     } else {
@@ -58,7 +58,7 @@ class loganDBClient {
   public function AddGetKey($key){
      array_push($this->keys,$key);     
   }
-  public function GetMultiKey($keystorename){
+  public function GetMultiKey($keystorename,$debug = false){
     $in = '|' . $this->apiversion . '|' 
         . $this->authid . '|' 
         . $this->sessionid . '|' 
@@ -70,7 +70,12 @@ class loganDBClient {
     }
     $in.= "|***LOGANDB_ENDLIST***"; 
     $in.= "|"; //end messsage string
-    return $this->WriteReadStream($in);
+    if($debug == true){
+      echo $in . "<br/>";
+    } else {
+      return $this->WriteReadStream($in);    
+    }
+     
   }
   public function SetMultiKey($keystorename){
     $in = '|' . $this->apiversion . '|' 
@@ -122,27 +127,33 @@ class loganDBClient {
     return $this->WriteReadStream($in);
   }
   public function WriteReadStream($in){
-    //echo $in . "<br/>";
-    $result = @fwrite($this->sock, $in);
     $endmarker = "***LOGANDBTCPENDSTREAM***";
-    $result = @fwrite($this->sock, $endmarker);   
-    $out = "";
-    $datalen = "";
-    while($datalen  == ""){
-      $datalen = @fread($this->sock, 9);
+    $in = $in . $endmarker;
+    $bytes = strlen($in);
+    $bytes = $bytes + 9;
+    str_pad($bytes,9,"0",STR_PAD_LEFT);
+    $in = $bytes . $in;
+    $result = fwrite($this->sock, $in);
+    if(true){
+        $out = "";
+        $datalen = "";
+        while($datalen  == ""){
+          $datalen = fread($this->sock, 9);
+        }
+        //echo "Data Length:" . $datalen . "<br/><br/>";
+        $discard = fread($this->sock, 1);
+        $reads = ceil(intval($datalen) / 1024);
+        //echo "Reads:" .  $reads . "<br/>";
+        for($readnum = 1; $readnum <= $reads;$readnum++){
+          $chunk = fread($this->sock, 1024);
+          $out .= $chunk;
+        }
+        $out = substr($out,0,(strlen($out)-1));
     }
-    $discard = @fread($this->sock, 1);
-    $reads = ceil(intval($datalen) / 1024);
-    //echo $reads . "<br/>";
-    for($readnum = 1; $readnum <= $reads;$readnum++){
-      $chunk = @fread($this->sock, 1024);
-      $out .= $chunk;
-    }
-    $out = substr($out,0,(strlen($out)-1));
     return $out; 
   }
   public function __destruct()
   {
-    @fclose($this->sock);
+    fclose($this->sock);
   }
 }
